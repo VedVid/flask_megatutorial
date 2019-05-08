3ad7af853e1f33ea3908570023f0e2aa371e419c
Vedor Vidurakis <toalak@gmail.com>
2019-05-08 19:03:09 +0200

    format using black

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,9 @@ class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    password2 = PasswordField(
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+    )
     submit = SubmitField("Register")
 
     def validate_username(self, username):
