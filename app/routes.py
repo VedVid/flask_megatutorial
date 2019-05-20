e4f4a541a87863650dde35d784adf05f4bf2d2b5
Vedor Vidurakis <toalak@gmail.com>
2019-05-20 17:39:19 +0200

    fixed pagination from chapter 9

--- app/routes.py
+++ app/routes.py
@@ -33,7 +33,7 @@ def index():
         page, app.config["POSTS_PER_PAGE"], False
     )
     next_url = url_for("index", page=posts.next_num) if posts.has_next else None
-    prev_url = url_for("index", page=posts.prev_num if posts.has_prev else None)
+    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
     return render_template(
         "index.html",
         title="Home",
@@ -52,16 +52,8 @@ def user(username):
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, app.config["POSTS_PER_PAGE"], False
     )
-    next_url = (
-        url_for("user", username=user.username, page=posts.next_num)
-        if posts.has_next
-        else None
-    )
-    prev_url = (
-        url_for("user", username=user.username, page=posts.prev_num)
-        if posts.has_prev
-        else None
-    )
+    next_url = url_for("user", username=user.username) if posts.has_next else None
+    prev_url = url_for("user", username=user.username) if posts.has_prev else None
     return render_template(
         "user.html", user=user, posts=posts.items, next_url=next_url, prev_url=prev_url
     )
@@ -129,7 +121,7 @@ def explore():
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config["POSTS_PER_PAGE"], False
     )
-    next_url = url_for("explore", page=posts.next_num) if posts.has_prev else None
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
     prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
     return render_template(
         "index.html",
