fba362987a520bebcad6ac57dcb6380f9915eb6d
Vedor Vidurakis <toalak@gmail.com>
2019-05-21 18:46:02 +0200

    Requesting a Password Reset

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,9 @@ from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.forms import LoginForm, ResetPasswordRequestForm, RegistrationForm, EditProfileForm, PostForm
 from app.models import User, Post
+from app.email import send_password_reset_email
 
 
 @app.before_request
@@ -81,6 +82,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.router("/reset_password_request", methods=["GET", "POST"])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash("Check your email for the instructions to reset your password")
+        return redirect(url_for("login"))
+    return render_template("reset_password_request.html", title="Reset Password", form=form)
 
 
 @app.route("/register", methods=["GET", "POST"])
